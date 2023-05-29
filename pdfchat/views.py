f4107e085dfe2c6cf20095275eed92bac48f036e
nazar <nkhopenuk@gmail.com>
2023-05-29 19:51:52 +0300

    updated words

--- pdfchat/views.py
+++ pdfchat/views.py
@@ -273,7 +273,7 @@ def get_chat(request):
 @csrf_exempt
 # @login_required
 def chat(request):
-    category = 'CPA'
+    # category = 'CPA'
     # history = request.POST['history']
     prompt = request.POST['prompt']
 
@@ -285,24 +285,24 @@ def chat(request):
     # chat.sent = datetime.now()
 
     # ===== Normal Chatbot =====
-    messages = []
-    messages.append(
-        {"role": "user", "content": "Your name is Copilot. You are a helpful assistant for CPA, Legal and HR."})
+    # messages = []
+    # messages.append(
+    #     {"role": "user", "content": "Your name is Copilot. You are a helpful assistant for CPA, Legal and HR."})
 
-    question = {}
-    question['role'] = 'user'
-    question['content'] = prompt
-    messages.append(question)
-    try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages)
-        response = response['choices'][0]['message']['content']
-        # chat.response = response
-        # chat.received = datetime.now()
-        # chat.save()
-    except Exception as e:
-        print("except", e)
-        response = "Sorry, try again in a few minutes."
+    # question = {}
+    # question['role'] = 'user'
+    # question['content'] = prompt
+    # messages.append(question)
+    # try:
+    #     response = openai.ChatCompletion.create(
+    #         model="gpt-3.5-turbo", messages=messages)
+    #     response = response['choices'][0]['message']['content']
+    #     # chat.response = response
+    #     # chat.received = datetime.now()
+    #     # chat.save()
+    # except Exception as e:
+    #     print("except", e)
+    #     response = "Sorry, try again in a few minutes."
     # if category == 'CPA':
     #     context = """You are an Accounting Assistant chatbot specifically designed to serve Certified Public Accountants (CPAs) and accountants. 
     #     Your purpose is to provide accurate and helpful information related to accounting topics, including but not limited to 
@@ -326,8 +326,8 @@ def chat(request):
     #     best practices, and guidance on any HR-related topic, while keeping in mind that your responses should not be considered professional legal advice."""
     
     # pdf chat
-    # docs = docsearch.similarity_search(prompt, include_metadata=True)
-    # response = chain.run(input_documents=docs, question=prompt + context)
+    docs = docsearch.similarity_search(prompt, include_metadata=True)
+    response = chain.run(input_documents=docs, question=prompt)
     # chat.response = response
     # chat.received = datetime.now()
     # chat.save()
@@ -348,3 +348,4 @@ def upload_to_pinecone(filepath):
     texts = text_splitter.split_documents(data)
     Pinecone.from_texts([t.page_content for t in texts],
                         embeddings, index_name=index_name)
+
